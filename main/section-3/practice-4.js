2868774c080b51a1cbf68cfdffab42ad3360dce3
tanglijin <1614468806@qq.com>
2019-07-14 00:48:50 +0800

    FATAL ERROR: CALL_AND_RETRY_LAST Allocation failed - JavaScript heap out of memory

--- main/section-3/practice-4.js
+++ main/section-3/practice-4.js
@@ -1,5 +1,43 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let collectionAChanged = [];
+  collectionA.forEach(i => {
+    if(i.length > 1){
+      collectionAChanged.push(i.split('-')[1]);
+    }
+    else {
+      collectionAChanged.push(i);
+    }
+  })
+  
+  let element = { };
+  let collectionACount = [];
+
+  let firstIndex = 0;
+  let lastIndex = 0;
+  let current = collectionAChanged[0];
+  while(lastIndex < collectionAChanged.length - 1){
+    firstIndex = collectionAChanged.indexOf(current);
+    lastIndex = collectionAChanged.lastIndexOf(current);
+    element.key = current;
+    element.count = lastIndex - firstIndex + 1;
+    collectionACount.push(element);
+    element = {};
+  }
+
+
+  let bKeys = objectB.value;
+  let A = collectionACount.map(i => {
+    // bKeys.includes(i.key) ? i.count -= i.count / 3 : i.count = i .count;
+    let temp = {};
+    temp.key = i.key;
+    if(bKeys.includes(i.key)){
+      temp.count = i.count - Math.floor(i.count / 3);
+    }else {
+      temp.count = i.count;
+    }
+    return temp;
+  });
+  return A;
 }
