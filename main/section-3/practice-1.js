4c169275c4ac451b2a47abc11568ca242b90a60f
tanglijin <1614468806@qq.com>
2019-07-13 23:45:45 +0800

    pass the practice1 test

--- main/section-3/practice-1.js
+++ main/section-3/practice-1.js
@@ -1,5 +1,35 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+
+  //collectionA.forEach(element -> );
+  let bKeys = objectB.value;
+  // let AkeyList = [];
+  // //bKeys.forEach(element => )
+  // //console.log(bKeys.map(i => objectB.values.includes(i) ? i - 1 : i));
+  // collectionA.forEach(i => {
+  //   AkeyList.push(i.key)
+  // });
+  //
+  //
+  //
+  // return  bKeys.map(i => objectB.values.includes(i) ? i - 1 : i);
+
+
+  // bKeys.filter(i => objectB.values.includes(i));
+  // return '实现练习要求，并改写该行代码。';
+  var result = [];
+  var temp = {};
+  collectionA.forEach(i => {
+    if(bKeys.includes(i.key)){
+      temp.key = i.key;
+      temp.count = i.count - 1;
+    }else {
+      temp.key = i.key;
+      temp.count = i.count;
+    }
+    result.push(temp);
+    temp = {};
+  })
+  return result;
 }
