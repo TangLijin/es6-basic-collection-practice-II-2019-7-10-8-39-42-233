e9dfac8abadcefd637a9d78f8b197793fe8cf9b7
tanglijin <1614468806@qq.com>
2019-07-14 00:41:32 +0800

    [err]I think the logic of my code is right but it failes to pass the test and say that 'JavaScript heap out of memory'

--- main/section-3/practice-3.js
+++ main/section-3/practice-3.js
@@ -1,5 +1,33 @@
 'use strict';
 
 module.exports = function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  let element = { };
+  let collectionACount = [];
+
+  let firstIndex = 0;
+  let lastIndex = 0;
+  let current = collectionA[0];
+  while(lastIndex < collectionA.length - 1){
+      firstIndex = collectionA.indexOf(current);
+      lastIndex = collectionA.lastIndexOf(current);
+      element.key = current;
+      element.count = lastIndex - firstIndex + 1;
+      collectionACount.push(element);
+      element = {};
+  }
+
+
+  let bKeys = objectB.value;
+  let A = collectionACount.map(i => {
+    // bKeys.includes(i.key) ? i.count -= i.count / 3 : i.count = i .count;
+    let temp = {};
+    temp.key = i.key;
+    if(bKeys.includes(i.key)){
+      temp.count = i.count - Math.floor(i.count / 3);
+    }else {
+      temp.count = i.count;
+    }
+    return temp;
+  });
+  return A;
 }
